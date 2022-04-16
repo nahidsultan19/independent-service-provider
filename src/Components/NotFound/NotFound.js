d6373bb2f89c6f7c7b942e6ddda49d6faa01f2b2
nahidsultan19 <nahidsultan38@gmail.com>
2022-04-17 01:16:05 +0600

    created fake data.

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            <h2>404, Not Found</h2>
+        <div className='container'>
+            <h2 className='text-center'>404, Not Found</h2>
         </div>
     );
 };
