d18b6409d8f5a4eacf3ad1167e4306871b6e1cb1
nahidsultan19 <nahidsultan38@gmail.com>
2022-04-17 14:26:54 +0600

    completed blogs.

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -6,27 +6,21 @@ const Blogs = () => {
         <div className='container'>
             <Accordion className='mt-5 w-75 mx-auto'>
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Header>Authorization vs Authentication</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Authorization is the process of verifying what the user has access to. On the other hand the authencitacaion is used to authenticate someone's identity.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header>Accordion Item #2</Accordion.Header>
+                    <Accordion.Header>Why are you using firebase? What other options do you have to implement authentication?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Firebase Authentication provide backend services, easy-to-use SDKs to authenticate users to our app.It supports authentication using email password, phone number, popular social like Google, Facebook, Twitter and more. We can use other options except firebase like- Auth0, MongoDB, Passport, Okta, Parse, Kinvey,Kuzzle, Hoodie,Heroku, etc.
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header>What other services does firebase provide other than authentication?</Accordion.Header>
+                    <Accordion.Body>
+                        There are many sevices which Firebase provides other than authentication, Such as - Cloud FireStore, Cloud Function, Hosting, Cloud Storage, Google Analytics, Machine learning Kit, Predictions, Cloud Messaging.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
